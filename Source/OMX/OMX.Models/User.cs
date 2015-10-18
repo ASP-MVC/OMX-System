56acd23051b3134d2a73fe5401fd111c4e1112c8
Domin1k <kristian.lubenov@gmail.com>
2015-10-18 23:43:20 +0300

    User register bug fixed

--- Source/OMX/OMX.Models/User.cs
+++ Source/OMX/OMX.Models/User.cs
@@ -1,5 +1,6 @@
 ﻿namespace OMX.Models
 {
+    using System;
     using System.Security.Claims;
     using System.Threading.Tasks;
 
@@ -7,9 +8,26 @@
     using Microsoft.AspNet.Identity.EntityFramework;
 
     using OMX.Contracts;
+    using OMX.Contracts.Models;
 
-    public class User : IdentityUser, IEntity
+    public class User : IdentityUser, IEntity, IAuditInfo, IDeletableEntity
     {
+        public User()
+        {
+            // This will prevent UserManager of causing exception when registering new user
+            this.CreatedOn = DateTime.Now;
+        }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
