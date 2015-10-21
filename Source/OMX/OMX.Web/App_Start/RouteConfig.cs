026f9bf1aaa04ed536d9ea6a79e41686f10ae6f9
Domin1k <kristian.lubenov@gmail.com>
2015-10-21 11:06:48 +0300

    Fix Routing exception with duplicate Controller names

--- Source/OMX/OMX.Web/App_Start/RouteConfig.cs
+++ Source/OMX/OMX.Web/App_Start/RouteConfig.cs
@@ -9,15 +9,15 @@
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "Default",
+            routes.MapRoute("Default",
                 "{controller}/{action}/{id}",
                 new
                 {
                     controller = "Home",
                     action = "Index",
                     id = UrlParameter.Optional
-                });
+                },
+                namespaces: new[] { "OMX.Web.Controllers" });
         }
     }
 }
---
+++ Source/OMX/OMX.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿namespace OMX.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using OMX.Data.UoW;
+
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IOMXData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult All()
+        {
+            return this.View();
+        }
+    }
+}
