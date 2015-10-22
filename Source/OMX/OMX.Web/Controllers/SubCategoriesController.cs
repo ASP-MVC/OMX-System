bad87e79f42864d7fc65495b5e4dfe156107e9f8
Domin1k <kristian.lubenov@gmail.com>
2015-10-22 22:45:35 +0300

    Add sub category

--- Source/OMX/OMX.Web/Controllers/SubCategoriesController.cs
+++ Source/OMX/OMX.Web/Controllers/SubCategoriesController.cs
@@ -1,14 +1,53 @@
 ﻿namespace OMX.Web.Controllers
 {
+    using System.Web.Mvc;
+
     using OMX.Data.UoW;
+    using OMX.Infrastructure.Populators;
+    using OMX.Models;
+    using OMX.Web.Models.BindingModels;
 
     public class SubCategoriesController : BaseController
     {
-        public SubCategoriesController(IOMXData data)
+        private IDropDownListPopulator populator;
+
+        public SubCategoriesController(IOMXData data, IDropDownListPopulator populator)
             : base(data)
         {
+            this.populator = populator;
         }
 
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create()
+        {
+            var subCatModel = new CreateSubCategoryBindingModel
+            {
+                Categories = this.populator.GetCategories()
+            };
 
+            return this.View(subCatModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateSubCategoryBindingModel model)
+        {
+            if(model != null && this.ModelState.IsValid)
+            {
+                var subCategory = new SubCategory
+                {
+                    Title = model.Title,
+                    CategoryId = model.CategoryId
+                };
+                this.Data.SubCategories.Add(subCategory);
+                this.Data.SaveChanges();
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            model.Categories = this.populator.GetCategories();
+            return this.View(model);
+        }
     }
 }
---
+++ Source/OMX/OMX.Web/Models/BindingModels/CreateSubCategoryBindingModel.cs
@@ -0,0 +1,25 @@
+﻿namespace OMX.Web.Models.BindingModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    using OMX.Infrastructure.Mapping;
+    using OMX.Models;
+
+    public class CreateSubCategoryBindingModel : IMapFrom<SubCategory>
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
+        [UIHint("SingleLineText")]
+        public string Title { get; set; }
+
+        [UIHint("DropDownList")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
