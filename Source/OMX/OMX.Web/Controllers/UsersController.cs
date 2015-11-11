bfd01e0ff2c8c7f8086bef19fcfbe019f3ead117
Domin1k <kristian.lubenov@gmail.com>
2015-11-11 13:12:56 +0200

    View my published ads implemented

--- Source/OMX/OMX.Web/Controllers/UsersController.cs
+++ Source/OMX/OMX.Web/Controllers/UsersController.cs
@@ -11,10 +11,12 @@
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.SignalR;
 
+    using OMX.Common;
     using OMX.Data.UoW;
     using OMX.Web.Hubs;
     using OMX.Web.Models.ViewModels;
 
+    using PagedList;
 
     [RoutePrefix("Users")]
     [System.Web.Mvc.Authorize]
@@ -103,6 +105,24 @@
                 .ProjectTo<CommentViewModel>()
                 .ToList();
             return this.View("DisplayTemplates/NotificationViewModel", notification);
+        }
+
+        [HttpGet]
+        [Route("MyAds")]
+        public ActionResult MyAds(int page = 1)
+        {
+            if (this.UserProfile != null)
+            {
+                var myAds =
+                    this.UserProfile.PublishedAds.Where(
+                        u => u.IsDeleted == false)
+                        .AsQueryable()
+                        .ProjectTo<AdViewModel>();
+
+                return this.View(myAds.ToPagedList(page, GlobalConstants.AdsPageSize));
+            }
+
+            return this.RedirectToAction("Login", "Account");
         }
 
         private void SaveFileToFileSystem(HttpPostedFileBase file)
