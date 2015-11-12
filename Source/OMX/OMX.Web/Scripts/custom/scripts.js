46b788016fbf8b2e25dfb9374fbe81b1b1c6f2f4
Domin1k <kristian.lubenov@gmail.com>
2015-11-12 19:01:41 +0200

    UI improvements

--- Source/OMX/OMX.Web/Scripts/custom/scripts.js
+++ Source/OMX/OMX.Web/Scripts/custom/scripts.js
@@ -26,6 +26,22 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#back-to-top").hide();
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    $('#back-to-top img').click(function () {
+        $('body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+});
+
 function ShowSuccessMsg(data) {
     notificationHelper.showSuccessMessage(data);
 }
